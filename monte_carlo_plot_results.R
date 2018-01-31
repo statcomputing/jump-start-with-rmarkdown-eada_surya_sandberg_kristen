08ec1b19df81f7d1107ef195d2a7f537994d9a1a
ksand3 <35612289+ksand3@users.noreply.github.com>
2018-01-31 13:11:21 -0500

    Delete monte_carlo_plot_results.R

--- monte_carlo_plot_results.R
+++
@@ -1,9 +0,0 @@
-library(reshape2)
-library(ggplot2)
-
-output_matrix <- monte_carlo_100_simulations()
-df<-data.frame(output_matrix)
-dfm <- melt(df, id.var=c("n_column","t_column"))
-
-p<-ggplot(dfm, aes(factor(t_column), value, colour=factor(n_column))) + geom_boxplot(width=0.7,position="dodge")+labs(x="t", y="Normal CDF",colour="n")+ggtitle("Boxplots of our Monte Carlo Simulation")
-print(p)
